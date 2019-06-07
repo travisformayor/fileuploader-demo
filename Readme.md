78ed3dbeb912abaff275725f7dc8222acba438fb
Travis <travisformayor@users.noreply.github.com>
2019-06-07 12:15:57 -0700

    boilerplate

---
+++ Readme.md
@@ -0,0 +1,41 @@
+# File Upload Example
+
+Code along with: <https://www.youtube.com/watch?v=b6Oe2puTdMQ&t=1s>
+
+```zsh
+touch server.js
+# makes the package.json
+npm init -y
+yarn add express express-fileupload
+# add dev dependencies
+yarn add -D nodemon concurrently
+```
+
+```json
+// package.json
+...
+"scripts": {
+  "start": "node server.js",
+  "server": "nodemon server.js",
+  "client": "npm start --prefix client",
+  "dev": "concurrently \"npm run server\" \"npm run client\"",
+}
+```
+
+Setup server.js, then
+
+```zsh
+create-react-app client
+```
+
+## Font Awesome React
+
+<https://github.com/FortAwesome/react-fontawesome#installation>
+
+in the react client folder...
+
+```zsh
+yarn add @fortawesome/fontawesome-svg-core
+yarn add @fortawesome/free-solid-svg-icons
+yarn add @fortawesome/react-fontawesome
+```
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+// Middleware ==== //
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({msg: 'No file uploaded'});
+  }
+  // get file...
+  const file = req.files.file;
+  // save it to public folder in react
+  // ToDo: add aws option here
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      // ToDo: have this pipe to react which turns it to errors message
+      return res.status(500).send(err);
+    }
+    // send successful upload info
+    res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+  });
+});
+
+// Start Dev Server
+app.listen(5000, () => console.log(`Started server`))
